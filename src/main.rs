4f725bef832a64404679018ecd71a855af0caacd
JockeW <wilhelmsson.joakim@gmail.com>
2023-05-10 17:05:29 +0200

    Added Scraper

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-use serde_json::{Result, Value};
-use html5ever::{parse_document};
-use html_parser::Dom;
+use scraper::{Html, Selector};
 use clap::{Arg, Command, ArgMatches};
-use reqwest::Client;
-use std::fs::{File, OpenOptions};
-use std::io::{BufRead, BufReader, Write};
-use std::path::PathBuf;
+use std::fs::OpenOptions;
 
 pub fn get_send_command() -> Command {
-    Command::new("send")
+    Command::new("scrape")
         .arg(
             Arg::new("url")
                 .short('u')
@@ -18,20 +13,22 @@ pub fn get_send_command() -> Command {
                 .required(true),
         )
         .arg(
-            Arg::new("load")
-                .short('l')
-                .long("load")
-                .value_name("request_name")
-                .help("Load a stored request and send that")
-                .required(false),
+            Arg::new("selectors")
+                .short('s')
+                .long("selectors")
+                .help("fghfgh")
+                .num_args(1..)
+                .required(true),
         )
         .arg(
-            Arg::new("save")
-                .short('s')
-                .long("save")
-                .help("Save the request to reuse it later")
+            Arg::new("keys")//headers?
+                .short('k')
+                .long("keys")
+                .help("qweqwe")
+                .num_args(1..)
                 .required(false),
         )
+        //.get_matches_from(itr)
 }
 
 fn main() {
@@ -58,7 +55,7 @@ fn main() {
         .get_matches();
 
     match matches.subcommand() {
-        Some(("send", cmd)) => send_command(cmd),
+        Some(("scrape", cmd)) => scrape(cmd),
         // Some(("push",   sub_c)) => {}, // push was used
         // Some(("commit", sub_c)) => {}, // commit was used
         _ => {} // Either no subcommand or one not tested for...
@@ -68,57 +65,61 @@ fn main() {
     if let Some(msg) = matches.get_one::<String>("test").map(|s| s.as_str()) {
         println!("Hello {msg}");
     }
-
-    // let client = Client::new();
-
-    // let method: &str = *matches.get_one("method").unwrap();
-    // let url: &str = *matches.get_one("url").unwrap();
-    //let output: PathBuf = matches.get_one("output").unwrap().map(|s: &str| PathBuf::from(s));
-
-    //let response = reqwest::blocking::get(url).unwrap().text().unwrap();
-
-    // if let Some(output) = output {
-    //     let mut file = File::create(output).unwrap();
-    //     response.copy_to(&mut file).unwrap();
-    // } else {
-    //     let body = response.text().unwrap();
-    //     println!("{}", body);
-    // }
-
-    // if let Some(name) = *matches.get_one("save").unwrap() {
-    //     save_command(name, method, url);
-    // }
 }
 
-fn send_command(args: &ArgMatches) {
-    println!("SEND SUB COMMAND");
+fn scrape(args: &ArgMatches) {
+    println!("SCRAPE SUB COMMAND");
     let url: String = args.get_one::<String>("url").unwrap().to_string();
-    let load = args.get_one::<String>("load");
-    if let Some(load) = load {
-        let load_str: &str = load;
-        //TODO: Load saved request and execute that
-    } else {
-        let response = reqwest::blocking::get(url).unwrap().text().unwrap();
-        let links = Dom::parse(&response).unwrap().to_json_pretty().unwrap();
-        //let test = serde_json::to_string_pretty(&response).unwrap();
-        println!("{}", response);
-    }
+    //selectors (String)
+    let mut selectors = args
+        .get_many::<String>("selectors")
+        .unwrap()
+        .map(|s| s.as_str());
 
-    let save = args.get_one::<bool>("save"); 
-    if let Some(s) = save {
-        let shoud_save = *s;
-        if shoud_save {//TODO: Also only save if the request was successful
-            println!("SAVE THE REQUEST. TODO: ADD ARGS FOR SAVING, LIKE NAME");
-        }
+    let test = selectors.next().unwrap();
+
+    //
+
+    let html = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let document = Html::parse_document(&html);
+    let selector = Selector::parse("h3.news-list__headline").unwrap();
+    //let select = document.select(&selector);
+    let content = document.select(&selector).map(|x| x.inner_html());
+    //println!("Select: {:?}", test);
+    for element in content {
+        println!("Content: {}", element);
     }
 }
+
+// fn send_command(args: &ArgMatches) {
+//     println!("SEND SUB COMMAND");
+//     let url: String = args.get_one::<String>("url").unwrap().to_string();
+//     let load = args.get_one::<String>("load");
+//     if let Some(load) = load {
+//         let load_str: &str = load;
+//         //TODO: Load saved request and execute that
+//     } else {
+//         let response = reqwest::blocking::get(url).unwrap().text().unwrap();
+//         let links = Dom::parse(&response).unwrap().to_json_pretty().unwrap();
+//         //let test = serde_json::to_string_pretty(&response).unwrap();
+//         println!("{}", response);
+//     }
+
+//     let save = args.get_one::<bool>("save"); 
+//     if let Some(s) = save {
+//         let shoud_save = *s;
+//         if shoud_save {//TODO: Also only save if the request was successful
+//             println!("SAVE THE REQUEST. TODO: ADD ARGS FOR SAVING, LIKE NAME");
+//         }
+//     }
+// }
 
 fn save_command(name: &str, method: &str, url: &str) {
-    let mut file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open("commands.txt")
-        .unwrap();
+    // let mut file = OpenOptions::new()
+    //     .create(true)
+    //     .append(true)
+    //     .open("commands.txt")
+    //     .unwrap();
 
-    writeln!(file, "{} {} {}", name, method, url).unwrap();
+    // writeln!(file, "{} {} {}", name, method, url).unwrap();
 }
