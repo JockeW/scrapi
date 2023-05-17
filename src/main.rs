8d6c73c2e25d8a860b1edb1fd516dcffdba35dc5
JockeW <wilhelmsson.joakim@gmail.com>
2023-05-17 07:40:57 +0200

    Working scrape_new using structs and enums for subcommands and args.

--- src/main.rs
+++ src/main.rs
@@ -119,7 +119,78 @@ struct Content {
 }
 
 fn scrape_new(url: String, selectors: Vec<String>, keys: Vec<String>, title: Option<String>) {
-    
+    if keys.len() != selectors.len() {
+        println!(
+            "{}: Keys needs to be as many as selectors",
+            "error".bold().color("red")
+        );
+        return;
+    }
+
+    let html = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let document = Html::parse_document(&html);
+
+    let mut contents: Vec<Vec<String>> = Vec::new();
+
+    for s in selectors {
+        println!("SELECTOR: {}", s);
+        let selector = Selector::parse(&s).expect("Not a valid selector");
+        let element_ref: Vec<ElementRef> = document.select(&selector).collect();
+
+        let mut content_vec: Vec<String> = Vec::new();
+
+        for element in element_ref {
+            let outer_text: Vec<&str> = element
+                .children()
+                .filter_map(|node| match node.value() {
+                    Node::Text(text) => Some(&text[..]),
+                    _ => None,
+                })
+                .collect();
+
+            //println!("{:?}", outer_text);
+            //TODO: Maybe add to get text of child nodes as well. (element.children())
+
+            let element_text: String = outer_text.join("");
+
+            content_vec.push(element_text);
+        }
+
+        contents.push(content_vec);
+    }
+
+let mut all_content: Vec<Vec<(&str, &str)>> = Vec::new();
+
+    for content_index in 0..contents.first().expect("NO CONTENT").len() {
+        let mut chunk: Vec<(&str, &str)> = Vec::new();
+        for (i, content) in contents.iter().enumerate() {
+            let header = keys[i].as_str();
+            let value = content[content_index].trim();
+            chunk.push((header, value));
+        }
+
+        all_content.push(chunk);
+    }
+
+    println!("CONTENT WITH KEYS: {:?}", all_content);
+
+    println!();
+
+    if let Some(title) = title {
+        println!("{}", title.bold());
+        println!();
+    }
+
+    for chunk in all_content {
+        //TODO: Print list or table. Just list for now
+        for data in chunk {
+            let header = data.0;
+            let value = data.1;
+            println!("{}: {}", header.bold(), value);
+        }
+        println!();
+    }
+
 }
 
 fn scrape(args: &ArgMatches) {
