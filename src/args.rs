003f06d7781e31c1c75813308be1962e232f3f4c
JockeW <wilhelmsson.joakim@gmail.com>
2023-08-22 23:38:15 +0200

    Work

--- src/args.rs
+++ src/args.rs
@@ -1,5 +1,4 @@
 use clap::{Args, Parser, Subcommand};
-use serde::{Serialize, Deserialize};
 use std::fmt;
 
 #[derive(Debug, Parser)]
@@ -13,17 +12,24 @@ pub struct RScrapeArgs {
 pub enum RScrapeCommand {
     /// Scrape a website and present the data
     Scrape(ScrapeCommand),
-    // Inspect saved scrapes
+
+    /// Check command, to check saved scrape and get all saved data presented nicely
+    Check(CheckCommand),
+
+    // Update saved scrape. Same args as Scrape command except save arg.
+    // Update(UpdateCommand)
+
+    // Scrape and inspect. Args for url, search and filter
     // Inspect(InspectCommand)
 
-    // Run saved scrape
-    // Run(RunCommand)
+    /// Run saved scrape
+    Run(RunCommand)
 
     // Delete saved scrape
     // Delete(DeleteCommand)
 }
 
-#[derive(clap::ValueEnum, Clone, Debug, PartialEq, Eq, Serialize)]
+#[derive(clap::ValueEnum, Clone, Debug, PartialEq, Eq)]
 pub enum Presentation {
     List,
     Table,
@@ -38,7 +44,7 @@ impl fmt::Display for Presentation {
     }
 }
 
-#[derive(Debug, Args, Serialize)]
+#[derive(Debug, Args)]
 pub struct ScrapeCommand {
     #[arg(short, long, required = true)]
     pub url: String,
@@ -54,38 +60,59 @@ pub struct ScrapeCommand {
     pub present: Option<Presentation>,
 }
 
-impl fmt::Display for ScrapeCommand {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        //TODO: Fix selectors and keys. Should not be printed as array.
-
-        let title;
-        match &self.title {
-            Some(t) => title = format!(" --title=\"{}\"",t.as_str()),
-            None => title = "".to_string(),
-        }
-
-        let save;
-        match &self.save {
-            Some(s) => save = format!(" --save\"{}\"",s.as_str()),
-            None => save = "".to_string(),
-        }
-
-        let present;
-        match &self.present {
-            Some(p) => {
-                if *p == Presentation::List {
-                    present = " --present=list";
-                } else {
-                    present = " --present=table";
-                }
-            },
-            None => present = "",
-        }
-
-        write!(
-            f,
-            "scrape --url={} --selectors={:?} --keys={:?}{}{}{}",
-            self.url, self.selectors, self.keys, title, save, present
-        )
-    }
+#[derive(Debug, Args)]
+pub struct CheckCommand {
+    #[arg(short, long, required = true)]
+    pub name: String,
 }
+
+#[derive(Debug, Args)]
+pub struct RunCommand {
+    #[arg(short, long, required = true)]
+    pub name: String,
+}
+
+// impl fmt::Display for ScrapeCommand {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         //TODO: Fix selectors and keys. Should not be printed as array!
+
+//         let selectors = self.selectors.iter().map(|val| format!("{}", val)).collect::<Vec<String>>().join(" ");
+//         // let mut selectors = String::new();
+//         // for selector in &self.selectors {
+//         //     let sel = format!("{} ", selector);
+//         //     selectors.push_str(selector);
+//         // }
+
+//         //let together: Vec<&str> = self.selectors.iter().map(|s| s.as_str()).collect();
+
+//         let title;
+//         match &self.title {
+//             Some(t) => title = format!(" --title=\"{}\"",t.as_str()),
+//             None => title = "".to_string(),
+//         }
+
+//         // let save;
+//         // match &self.save {
+//         //     Some(s) => save = format!(" --save\"{}\"",s.as_str()),
+//         //     None => save = "".to_string(),
+//         // }
+
+//         let present;
+//         match &self.present {
+//             Some(p) => {
+//                 if *p == Presentation::List {
+//                     present = " --present list";
+//                 } else {
+//                     present = " --present table";
+//                 }
+//             },
+//             None => present = "",
+//         }
+
+//         write!(
+//             f,
+//             "scrape --url {} --selectors {} --keys {:?}{}{}",
+//             self.url, selectors, self.keys, title, present
+//         )
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,111 @@
 mod args;
 
 use args::{Presentation, RScrapeArgs};
-use clap::{Arg, ArgMatches, Command, Parser};
-use cli_table::{format::Justify, print_stdout, Cell, Style, Table};
+use clap::Parser;
+use cli_table::{Cell, Style, Table};
 use colored::Colorize;
-use inquire::{Confirm, Text};
+use inquire::Confirm;
 use scraper::{ElementRef, Html, Node, Selector};
 use std::fs::OpenOptions;
-use std::io::{BufRead, BufReader, Read, Write};
+use std::io::{BufRead, BufReader, Write};
 
 fn main() {
     let args = RScrapeArgs::parse();
 
     match args.sub_command {
         args::RScrapeCommand::Scrape(cmd) => scrape(
-            cmd.to_string(),
             cmd.url,
             cmd.selectors,
             cmd.keys,
             cmd.title,
             cmd.save,
-            cmd.present
+            cmd.present,
         ),
+        args::RScrapeCommand::Check(cmd) => check(cmd.name),
+        args::RScrapeCommand::Run(cmd) => run(cmd.name),
         _ => {}
     }
 }
 
+fn get_saved_scrape(name: &str) -> Option<&'static str> {
+    let file_content = include_str!("../scrapes.txt");
+
+    for line in file_content.trim().split('\n') {
+        let scrape_name = line.split(';').collect::<Vec<&str>>()[0];
+        if scrape_name == name {
+            return Some(line);
+        }
+    }
+
+    None
+}
+
+fn run(name: String) {
+    let scrape_data = get_saved_scrape(&name);
+
+    match scrape_data {
+        Some(data) => scrape_saved_scrape(data),
+        None => println!("Scrape {} not found!", &name),
+    }
+
+    // scrape(url, selectors, keys, title, save, present)
+}
+
+fn scrape_saved_scrape(data_str: &str) {
+    let data: Vec<&str> = data_str.split(";").collect();
+
+    let url = data[1];
+    let selectors = data[2][1..data[2].len() - 1]
+        .split(", ")
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|&s| s.trim().replace("\"", "").to_string())
+        .collect::<Vec<String>>();
+
+    println!("SELECTORS: {:?}", selectors);
+
+    let keys = data[3][1..data[3].len() - 1]
+        .split(", ")
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|&s| s.trim().replace("\"", "").to_string())
+        .collect::<Vec<String>>();
+
+    println!("KEYS: {:?}", keys);
+
+    scrape(url.to_string(), selectors, keys, None, None, None);
+}
+
+fn check(name: String) {
+    let scrape_data = get_saved_scrape(&name);
+
+    match scrape_data {
+        Some(data) => print_scrape_info(data),
+        None => println!("Scrape {} not found!", &name),
+    }
+}
+
+fn print_scrape_info(data_str: &str) {
+    let data: Vec<&str> = data_str.split(";").collect();
+
+    println!("Name: {}", data[0]);
+    println!("Url: {}", data[1]);
+    println!("Selectors: {}", data[2]);
+    println!("Keys: {}", data[3]);
+
+    println!("Title: {}", data[4]);
+    println!("Present: {}", data[5]);
+
+    println!("Full command: TODO!"); //TODO: Implement a print of the full command
+}
+
 fn scrape(
-    input: String,
     url: String,
     selectors: Vec<String>,
     keys: Vec<String>,
     title: Option<String>,
     save: Option<String>,
-    present: Option<Presentation>
+    present: Option<Presentation>,
 ) {
     if keys.len() != selectors.len() {
         println!(
@@ -42,8 +114,6 @@ fn scrape(
         );
         return;
     }
-
-    //println!("Full command: {}", input);
 
     let html = reqwest::blocking::get(&url).unwrap().text().unwrap();
     let document = Html::parse_document(&html);
@@ -95,12 +165,16 @@ fn scrape(
         println!();
     }
 
+    let mut selected_presentation = Presentation::List;
+
     match present {
         Some(Presentation::List) => {
             print_content_list(all_content, keys.iter().map(|k| k.as_str()).collect());
+            selected_presentation = Presentation::List;
         }
         Some(Presentation::Table) => {
             print_content_table(all_content, keys.iter().map(|k| k.as_str()).collect());
+            selected_presentation = Presentation::Table;
         }
         None => {
             //Printing list as default. TODO: Maybe prompt and ask for list or table in this case instead.
@@ -118,7 +192,7 @@ fn scrape(
                 .prompt();
 
             match answer {
-                Ok(true) => save_scrape(&save, &input, &url, selectors, keys, title, present),
+                Ok(true) => save_scrape(&save, &url, selectors, keys, title, selected_presentation),
                 Ok(false) => println!("Skipped saving"),
                 Err(_) => println!("Error with questionnaire, try again later"),
             }
@@ -150,19 +224,18 @@ fn print_content_table(content: Vec<Vec<&str>>, keys: Vec<&str>) {
 
 fn save_scrape(
     name: &str,
-    full_command: &str,
     url: &str,
     selectors: Vec<String>,
     keys: Vec<String>,
     title: Option<String>,
-    present: Option<Presentation>,
+    present: Presentation,
 ) {
     println!("Saving scrape...");
     let mut file = OpenOptions::new()
         .create(true)
         .append(true)
         .read(true)
-        .open("scrapes.json")
+        .open("scrapes.txt")
         .unwrap();
 
     let buff_reader = BufReader::new(&file);
@@ -179,7 +252,7 @@ fn save_scrape(
 
     let scrape_names: Vec<&str> = saved_scrapes
         .iter()
-        .map(|s| s.split('|').collect::<Vec<&str>>()[0])
+        .map(|s| s.split(';').collect::<Vec<&str>>()[0])
         .collect();
 
     println!("SCRAPE NAMES: {:?}", scrape_names);
@@ -188,15 +261,16 @@ fn save_scrape(
         println!("There is already a scrape with that name")
         //TODO: Prompt user with options for entering a new name or cancel
     } else {
+        let title_to_write = match title {
+            Some(t) => t,
+            None => "".to_string(),
+        };
+
         writeln!(
             file,
-            "{}|{}|{}|{:?}|{:?}|{:?}|{:?}",
-            name, full_command, url, selectors, keys, title, present
+            "{};{};{:?};{:?};{};{}",
+            name, url, selectors, keys, title_to_write, present
         )
         .unwrap();
     }
-
-    //TODO: Maybe store scrape as JSON. Could possibly be easier to combine scrapers later, and read scrapers from the file etc. See scrapers.json file
-
-    //TODO: Store full command (input) as one field
 }
