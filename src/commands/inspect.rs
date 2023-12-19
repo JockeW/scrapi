7aae56863685d7ba9f3269c72bc5e381dbc93e18
JockeW <wilhelmsson.joakim@gmail.com>
2023-12-19 22:03:46 +0100

    Added thirtyfour and Tokio

--- src/commands/inspect.rs
+++ src/commands/inspect.rs
@@ -1,21 +1,33 @@
-use scraper::{Html, Selector, ElementRef};
+use scraper::{ElementRef, Html, Selector};
+use thirtyfour::{DesiredCapabilities, WebDriver, WebDriverCommands};
 
-pub fn inspect(url: String, filter: Option<String>, search: Option<String>) {
-    let html = reqwest::blocking::get(&url).unwrap().text().unwrap();
-    let document = Html::parse_document(&html);
+#[tokio::main]
+pub async fn inspect(url: String, filter: Option<String>, search: Option<String>) {
+    // let html = reqwest::blocking::get(&url).unwrap().text().unwrap();
+    // let document = Html::parse_document(&html);
 
-    if let Some(filter) = filter {
-        let selector = Selector::parse(&filter).expect("Not a valid selector");
-        let element_ref: Vec<ElementRef> = document.select(&selector).collect();
+    let caps = DesiredCapabilities::chrome();
+    let driver = WebDriver::new("http://localhost:4444", &caps).await.unwrap();
 
-        for element in element_ref {
-            let test = element.html();
-            println!("{}", test);
-        }
-    }
+    // Navigate to https://wikipedia.org.
+    driver.get(url).await.unwrap();
 
-    println!("{}", document.html());
-    if let Some(search) = search {
-        document.html().
-    }
+    let test = driver.find_element(thirtyfour::By::Name("html")).await.unwrap();
+    println!("TESTING: {}", test.inner_html().await.unwrap());
+    // if let Some(filter) = filter {
+    //     let selector = Selector::parse(&filter).expect("Not a valid selector");
+    //     let element_ref: Vec<ElementRef> = document.select(&selector).collect();
+
+    //     for element in element_ref {
+    //         let test = element.html();
+    //         println!("{}", test);
+    //     }
+    // }
+
+    // println!("{}", document.html());
+    // if let Some(search) = search {
+    //     document.html().
+    // }
+
+    driver.quit().await.unwrap();
 }
